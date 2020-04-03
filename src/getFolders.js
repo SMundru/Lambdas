838d34ce8d23a5c65bac391b352dfa9e31e7fb77
Sivan Mundru <sivan.mundru@leadx.com>
2020-04-03 17:44:44 +0100

    lambda updated

--- src/getFolders.js
+++ src/getFolders.js
@@ -1,27 +1,63 @@
 const AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+const s3 = new AWS.S3();
+const dynamoDb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 exports.handler = (event, context, callback) => {
     return new Promise((resolve, reject) => {
+        const {bucket, folder, delimiter} = event.queryStringParameters;
         const s3params = {
-            Bucket: event.queryStringParameters.bucket,
+            Bucket: bucket,
             MaxKeys: 1000,
-            Delimiter: '/',
-            Prefix: event.queryStringParameters.folder
+            Delimiter: delimiter,
+            Prefix: folder
         };
         s3.listObjectsV2(s3params, (err, data) => {
             if (err) {
                 reject(err);
             } else {
-                let response = {
-                    "statusCode": 200,
-                    "headers": {
-                        "Access-Control-Allow-Origin": "*"
-                    },
-                    "body": JSON.stringify(data),
-                    "isBase64Encoded": false
+                const keys = data.Contents.map(c => {
+                    if (c.Key !== folder) {
+                        return c.Key;
+                    }
+                }).filter(x => x);
+
+                let Keys = keys.map(x => {
+                    const key = x.split(".")[0];
+                    return {
+                        "name": {
+                            S: `${key}`
+                        }
+                    }
+                });
+
+                const dynamoRequest = {
+                    RequestItems: {
+                        "VideoInfo": {Keys}
+                    }
                 };
-                callback(null, response);
+
+                dynamoDb.batchGetItem(dynamoRequest, (error, output) => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        const result = {};
+                        keys.map(k => {
+                            let element = output.Responses.VideoInfo.filter(r => k.indexOf(r.name.S) !== -1)[0];
+                            result[k] = typeof element !== 'undefined' ? element : {};
+                        });
+                        let response = {
+                            "statusCode": 200,
+                            "headers": {
+                                "Access-Control-Allow-Origin": "*"
+                            },
+                            "body": JSON.stringify(result),
+                            "isBase64Encoded": false
+                        };
+                        callback(null, response);
+                    }
+                });
+
             }
         });
-    });
+    })
+        ;
 };
